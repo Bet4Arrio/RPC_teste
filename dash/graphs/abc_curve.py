682f7afec32d035e773407e4feefa89fa797b702
Emanuel Silva <emanuelsa-rv@hotmail.com>
2022-08-13 20:38:40 -0300

    Fix: returns types

--- dash/graphs/abc_curve.py
+++ dash/graphs/abc_curve.py
@@ -5,22 +5,7 @@ from plotly.subplots import make_subplots
 
 
 
-def SetColor(y):
-    if(y == "A"):
-        return "#fd084a"
-    elif(y == "B"):
-        return "#494fc1"
-    return "#a0fefa"
-
-def SetColor2(y):
-    if(y == "A"):
-        return "#ff0000"
-    elif(y == "B"):
-        return "#004941"
-    return "#00fdf0"
-
-
-def abc_pie(df: go.Figure, class_column:str, max_column:str):
+def abc_pie(df: go.Figure, class_column:str, max_column:str) -> go.Figure:
     values2 = df.groupby(class_column).sum().reset_index()
 
     return px.pie(values2, values=max_column, names=class_column, title='Representação porcentural dos grupos')
